aa8e2fe0c0bf38d2b5e71cfd45bfcbc70c22d3d5
breno <brenondss@gmail.com>
2023-06-01 10:35:06 -0300

    Fix(account page): Fix IU problems and improve responsiveness

--- itm-website/src/pages/account/Account.tsx
+++ itm-website/src/pages/account/Account.tsx
@@ -2,6 +2,8 @@ import './style.css'
 import { Formik, Form, Field } from 'formik';
 import user from '../../assets/user.svg'
 import wave from '../../assets/wave.svg'
+import { Input } from '@mui/material';
+
 
 function Account() {
 
@@ -29,12 +31,9 @@ function Account() {
     <div className='account-wrapper'>
       <div className='account-card'>
         <div className='title-wrapper'>
-          <h1 className='login-title'>LOGIN</h1>
-          <div>
+          <h1 className='login-title text-4xl mt-5'>LOGIN</h1>
           <p className='client-p'>sou cliente</p>
-        <img className='user-svg' src={user}></img>
-
-          </div>
+          <img className='user-svg' src={user}></img>
         </div>
         <Formik
           initialValues={{ email: '', password: '' }}
@@ -43,21 +42,30 @@ function Account() {
           }}
         >
           {({ values, errors, touched }) => (
-            <Form>
-              <div className='input-wrapper'>
-                <Field type='email' name='email' placeholder='Email' />
+            <Form className='w-full'>
+              <div>
+                <Field 
+                className='ml-[28px] bg-transparent border-b-[1px] border-b-zinc-200 w-[80%] text-[14px] p-6 mb-6 text-slate-50 font-[Roboto]'
+                type='email'
+                spellCheck={false}
+                name='email' 
+                placeholder='EMAIL' />
                 {errors.email && touched.email && (
                   <div className='error-message'>{errors.email}</div>
                 )}
               </div>
-              <div className='input-wrapper'>
-                <Field type='password' name='password' placeholder='Senha' />
+              <div className='w-full'>
+                <Field 
+                className='ml-[28px] bg-transparent border-b-[1px] border-b-zinc-200 w-[80%] text-2xl p-6 text-slate-50'
+                type='password' 
+                name='password' 
+                placeholder='SENHA' />
                 {errors.password && touched.password && (
                   <div className='error-message'>{errors.password}</div>
                 )}
               </div>
-              <button className='sign-in' type='submit'>Entrar</button>
-              <button className='sign-up' type='submit'>Crie sua conta</button>
+              <button className='sign-in text-2xl ml-[10px]' type='submit'>Entrar</button>
+              <button className='sign-up ml-[10px]' type='submit'>Crie sua conta</button>
             </Form>
           )}
         </Formik>
