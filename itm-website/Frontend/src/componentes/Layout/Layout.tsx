4d71c208d67d34536bf6d795ef6e03488ca201d0
breno <brenondss@gmail.com>
2023-06-28 13:45:07 -0300

    Fix(burguer open): Fix button child of a button problem

--- itm-website/Frontend/src/componentes/Layout/Layout.tsx
+++ itm-website/Frontend/src/componentes/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import './style.css'
 import Footer from "../Footer/Footer";
 import Header from "../Header/HeaderComponent/Header";
@@ -6,6 +6,28 @@ import Whatsapp from "../WhatsApp/Whatsapp";
 import BannerCard from '../MarketingCard/GameCard/BannerCard';
 
 function Layout({ children }: { children: ReactNode }) {
+  
+  // const getSession = async () => {
+  //   var myHeaders = new Headers();
+  //   myHeaders.append("charset", "UTF-8");
+  //   myHeaders.append("Content-Type", "application/json");
+  //   myHeaders.append("login", "userapi");
+  //   myHeaders.append("senha", "2f8265a");
+
+  //   fetch("http://itm.objectdata.com.br/api/", {
+  //     method: 'POST',
+  //     headers: myHeaders,
+  //     redirect: 'follow'
+  //   })
+  //     .then(response => response.text())
+  //     .then(result => console.log(result))
+  //     .catch(error => console.log('error', error));
+  //     }
+
+  //   useEffect(() => {
+  //     getSession()
+  //   }, [])
+
     return (
       <div className='layout-component'>
         <Header />
