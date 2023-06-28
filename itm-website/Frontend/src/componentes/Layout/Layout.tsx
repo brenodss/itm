199fd5dd2bc180e10210b274354db12c98981420
breno <brenondss@gmail.com>
2023-06-28 13:47:27 -0300

    Chorus(Layout): Remove comments

--- itm-website/Frontend/src/componentes/Layout/Layout.tsx
+++ itm-website/Frontend/src/componentes/Layout/Layout.tsx
@@ -7,27 +7,6 @@ import BannerCard from '../MarketingCard/GameCard/BannerCard';
 
 function Layout({ children }: { children: ReactNode }) {
   
-  // const getSession = async () => {
-  //   var myHeaders = new Headers();
-  //   myHeaders.append("charset", "UTF-8");
-  //   myHeaders.append("Content-Type", "application/json");
-  //   myHeaders.append("login", "userapi");
-  //   myHeaders.append("senha", "2f8265a");
-
-  //   fetch("http://itm.objectdata.com.br/api/", {
-  //     method: 'POST',
-  //     headers: myHeaders,
-  //     redirect: 'follow'
-  //   })
-  //     .then(response => response.text())
-  //     .then(result => console.log(result))
-  //     .catch(error => console.log('error', error));
-  //     }
-
-  //   useEffect(() => {
-  //     getSession()
-  //   }, [])
-
     return (
       <div className='layout-component'>
         <Header />
