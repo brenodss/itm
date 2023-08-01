c9bcd44bae52ba4194c7b38bf9102280d05edb42
brenondss <brenondss@gmail.com>
2023-08-01 11:38:41 -0300

    Style(Dynamic SearchBar): Add hr on each item and add price

--- itm-website/Frontend/src/componentes/Header/SearchBar/CustomSearchBar.tsx
+++ itm-website/Frontend/src/componentes/Header/SearchBar/CustomSearchBar.tsx
@@ -1,6 +1,5 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import Select from 'react-select';
-import { components } from 'react-select';
 import './style.css';
 import useDynamicSearchWithTimer from './useDynamicSearch';
 
@@ -10,28 +9,31 @@ const CustomSearchBar = ({}) => {
   const {filteredResults, isLoading} = useDynamicSearchWithTimer(productString);
 
   const CustomOption = (props: any) => (
+    <div className='mt-5 mb-5'>
+
     <button onClick={() => setProductString(props.data.descricao)} >
-      <div className='flex flex-row items-center'>
-        <div className='w-40 h-40'>
+      <div className='flex flex-row items-center flex-start text-start'>
           <img
-            className='w-full h-full object-contain'
+            className='w-40 h-40 object-contain'
             src={`http://itm.objectdata.com.br${props.data.imagem_pequena}`}
             alt='produto-image'
           />
-        </div>
-        <div className='flex flex-col'>
+        <div className='flex flex-col ml-10'>
           <span>{props.data.descricao}</span>
-          <span>{props.data.preço}</span>
+          <span className='text-[#ff5510]'>{`${props.data.preco}R$`}</span>
         </div>
       </div>
     </button>
+    <hr className='border border-[#ddd] w-full'></hr>
+
+    </div>
     
   );
 
   return (
     <div className='search-wraper'>
       <Select
-        className='basic-single text-2xl font-bold'
+        className='basic-single text-2xl font-bold border-none'
         classNamePrefix='color-[#ddd]'
         placeholder='O que está procurando?'
         styles={{
@@ -46,7 +48,7 @@ const CustomSearchBar = ({}) => {
 
           control: (baseStyles, _state) => ({
             ...baseStyles,
-
+            borderStyle: '1px solid white',
             '@media (min-width: 1049px)': {
               backgroundColor: '#ddd'
              },
@@ -58,7 +60,8 @@ const CustomSearchBar = ({}) => {
 
         isLoading={isLoading}
         isSearchable
-
+        
+        blurInputOnSelect={true}
         inputValue={productString}
         onInputChange={(inputValue: string) => setProductString(inputValue)}
         
